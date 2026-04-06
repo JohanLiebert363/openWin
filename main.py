3bb47eb83bfdb49e13f09549270a4e2398f0d5af
Duo <edintahiri.2013@gmail.com>
2026-04-06 18:20:39 +0200

    Refactor main.py to simplify command options

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
 import subprocess
 from pathlib import Path
 import sys
+from rich import print
+from rich.prompt import Prompt
+from pyfiglet import Figlet
+
 
 script_path = Path(__file__).resolve()
 directory = script_path.parent
+f = Figlet(font='slant')            # try 'slant','banner','standard'
+print(f.renderText('WinOpen'))
 
-def find_file(directory):
-     
-
-     print(directory
-     )
-
-def open_cmd(ask, directory):
-    for i in range(ask):
-        print(f"Opening cmd window {i+1}...")
-        subprocess.Popen(f'start cmd /K "dir /s "{directory}""', shell=True)
-
-def run_file(script_path):
-
-    subprocess.Popen(f'start cmd /K "{sys.executable} "{script_path}""', shell=True)
-
+print("[cyan]What to run[/cyan]\n")
+ask = Prompt.ask("[red]>>> [/red]")
+print("[cyan]Where is it (directory)[/cyan]\n")
+ask2 = Prompt.ask("[red]>>> [/red]")
+def open_cmd(ask, ask2):
+    pass
 def main():
-    print("\n--- OpenWin ---")
-    options = ["1. find file", "2. run file", "3. open cmd", "4. run all"]
+
+
+    options = ["[blue]1. run all[/blue]"]
     print("\n".join(options))
     
     ask_command = input("What to run (type the number) >>> ").strip()
 
 
     if ask_command in ["1", "1."]:
-        find_file(directory)
-        print(directory)
-    elif ask_command in ["2", "2."]:
-        run_file(script_path)
-    elif ask_command in ["3", "3."]:
-        ask = int(input("How many times to open cmd >> "))
-        open_cmd(ask, directory)
-    elif ask_command in ["4", "4."]:
-        ask = int(input("How many times to open cmd >> "))
-        find_file(directory)
-        run_file(script_path)
-        open_cmd(ask, directory)
+        open_cmd(ask, directory, script_path)
     else:
         print("Invalid option.")
 
